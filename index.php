ee23c645405a8b9a3ed3d6d99b31025526fa248f
dieromeo <diegoromeo99@icloud.com>
2024-01-15 16:10:55 +0100

    feat: bonus 1

--- index.php
+++ index.php
@@ -6,13 +6,13 @@
 class Movie
 {
     public $titolo;
-    public $genere;
+    public array $generi = [];
     public $durata;
 
     /**
      * __construct
      *
-     * @param  mixed $_titolo del film
+     * @param  mixed $_titolo
      * @param  mixed $_genere
      * @param  mixed $_durata
      * @return void
@@ -20,18 +20,24 @@ class Movie
     function __construct($_titolo, $_genere, $_durata)
     {
         $this->titolo = $_titolo;
-        $this->genere = $_genere;
+        if (is_array($_genere) && !empty($_genere)) {
+            $this->generi = $_genere;
+        }
         $this->durata = $_durata;
     }
 
     /**
-     * getGenereToLower ritorna il genere del film in minuscolo
+     * getGenereToLower ritorna un array contenente i generi in minuscolo
      *
      * @return string
      */
-    private function getGenereToLower()
+    public function getGenereToLower()
     {
-        return strtolower($this->genere);
+        $generi_minuscoli = [];
+        foreach ($this->generi as $genere_singolo) {
+            $generi_minuscoli[] = strtolower($genere_singolo);
+        }
+        return $generi_minuscoli;
     }
 
     /**
@@ -42,20 +48,32 @@ class Movie
     public function suggerimentoEta()
     {
         $generi_per_adulti = ["horror", "thriller", "guerra"];
-        if (in_array($this->getGenereToLower(), $generi_per_adulti)) {
-            echo "Film consigliato ad un pubblico maggiorenne";
+        $generi_film = $this->getGenereToLower();
+        if (!empty(array_intersect($generi_per_adulti, $generi_film))) {
+            echo "Visione consigliata ad un solo pubblico maggiorenne";
+        } else {
+            echo "E' un film adatto a tutti";
         }
     }
 }
 
-$faf = new Movie("Fast and Furious", "Azione", "2h.55m");
-$lotr = new Movie("Il Signore degli Anelli", "Fantasy", "3h.14m");
+$faf = new Movie("Fast and Furious", ["azione"], "2h.55m");
+$lotr = new Movie("Il Signore degli Anelli", ["Fantasy", "Azione"], "3h.14m");
 
 $films = [$faf, $lotr];
 
 foreach ($films as $film) {
     echo "Il titolo è: " . $film->titolo . "<br>";
-    echo "Il genere è: " . $film->genere . "<br>";
+    if (count($film->generi) > 1) {
+        echo "I generi sono: ";
+    } else {
+        echo "Il genere è: ";
+    }
+
+    foreach ($film->generi as $genere) {
+        echo $genere . " ";
+    };
+    echo "<br>";
     echo "La durata è: " . $film->durata . "<br>";
     echo "<hr>";
 }
